2a2c5e8457dc91f6fb831429468a65830b0b519c
IcedComputer <31418197+IcedComputer@users.noreply.github.com>
2018-08-17 10:23:19 -0700

    remove sudo

--- updates.sh
+++ updates.sh
@@ -9,54 +9,54 @@ PIDIR=/etc/pihole
 function download()
 {
 
-#adlists.list 
-sudo wget -O $TEMPDIR/adlists.list 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/lists'
+	#adlists.list 
+	wget -O $TEMPDIR/adlists.list 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/lists'
 
-# Regex Lists
+	# Regex Lists
 
-sudo wget -O $TEMPDIR/regex.download 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/regex.list'
-wait
-sudo wget -O $TEMPDIR/regex.country 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/country_regex'
-wait
-sudo wget -O $TEMPDIR/regex.oTLD 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/oTLD_regex'
-wait
-sudo wget -O $TEMPDIR/regex.uslocal 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/uslocal'
+	wget -O $TEMPDIR/regex.download 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/regex.list'
+	wait
+	wget -O $TEMPDIR/regex.country 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/country_regex'
+	wait
+	wget -O $TEMPDIR/regex.oTLD 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/oTLD_regex'
+	wait
+	wget -O $TEMPDIR/regex.uslocal 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/uslocal'
 }
 
 function modify()
 {
 
-sudo cat $TEMPDIR/regex.download $TEMPDIR/regex.country $TEMPDIR/regex.oTLD $TEMPDIR/regex.uslocal | grep -v '#' | sort | uniq > $TEMPDIR/regex.list
-wait
+	cat $TEMPDIR/regex.download $TEMPDIR/regex.country $TEMPDIR/regex.oTLD $TEMPDIR/regex.uslocal | grep -v '#' | sort | uniq > $TEMPDIR/regex.list
+	wait
 }
 
 
 #move
 function move()
 {
-#adlists.list
-sudo mv $TEMPDIR/adlists.list $PIDIR/adlists.list
-#Wildcards (03-pihole-wildcard.conf)
-sudo mv $TEMPDIR/regex.list  $PIDIR/regex.list
+	#adlists.list
+	mv $TEMPDIR/adlists.list $PIDIR/adlists.list
+	#Wildcards (03-pihole-wildcard.conf)
+	mv $TEMPDIR/regex.list  $PIDIR/regex.list
 
 }
 
 #cleanup
 function clean()
 {
-sudo rm -f $TEMPDIR/regex.download
-sudo rm -f $TEMPDIR/regex.country
-sudo rm -f $TEMPDIR/regex.oTLD
-sudo rm -f $TEMPDIR/regex.uslocal
+ rm -f $TEMPDIR/regex.download
+ rm -f $TEMPDIR/regex.country
+ rm -f $TEMPDIR/regex.oTLD
+ rm -f $TEMPDIR/regex.uslocal
 }
 
 function scripts()
 {
-sudo bash /scripts/Finished/ListUpdater.sh
-wait
-sudo killall -SIGHUP pihole-FTL
-wait
-sudo pihole -g
+ bash /scripts/Finished/ListUpdater.sh
+ wait
+ killall -SIGHUP pihole-FTL
+ wait
+ pihole -g
 }
 
 download
