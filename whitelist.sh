667cc574cda6132f77f88b2fa23047e35483c6cc
IcedComputer <31418197+IcedComputer@users.noreply.github.com>
2020-03-09 12:52:58 -0700

    Update whitelist.sh

--- whitelist.sh
+++ whitelist.sh
@@ -11,4 +11,10 @@ PIDIR=/etc/pihole
 
 #whitelist 
 wget -O $TEMPDIR/whitelist.temp 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/whitelist.txt'
-mv $TEMPDIR/whitelist.temp $PIDIR/whitelist.txt
+cp $PIDIR/whitelist.txt $TEMPDIR/current.wl.temp
+
+cat $TEMPDIR/current.wl.temp $TEMPDIR/whitelist.temp | sort | uniq > $TEMPDIR/final.wl.temp
+mv $TEMPDIR/final.wl.temp $PIDIR/whitelist.txt
+
+rm $TEMPDIR/current.wl.temp
+rm $TEMPDIR/whitelist.temp
