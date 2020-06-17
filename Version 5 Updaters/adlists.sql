48da90c3551a4c3fee8740f45d768780ad7715c9
IcedComputer <31418197+IcedComputer@users.noreply.github.com>
2020-06-17 13:15:10 -0700

    Update adlists.sql

--- Version 5 Updaters/adlists.sql
+++ Version 5 Updaters/adlists.sql
@@ -1,5 +1,5 @@
 CREATE TEMP TABLE i(txt);
 .separator ~
-.import /etc/pihole/adlist.list i
+.import /etc/pihole/adlists.list i
 INSERT OR IGNORE INTO adlist (address) SELECT txt FROM i;
 DROP TABLE i;
